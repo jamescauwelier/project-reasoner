1f1244c6c8e656ab313ba1ae78c76ef5756ff536
James Cauwelier <james@accelerated.dev>
2025-12-10 10:19:48 -0800

    CRUD functions for knowledge graph domain events

--- project_reasoner/domain/event.py
+++ project_reasoner/domain/event.py
@@ -1,6 +1,8 @@
+import uuid
 from dataclasses import dataclass
+from typing import Callable, List
 
-from rdflib import URIRef
+from rdflib import URIRef, Graph, Literal, RDF
 
 from project_reasoner.ref import ref
 
@@ -13,3 +15,53 @@ class DomainEvent:
 
 def domain_event_ref(domain_event_id: str) -> URIRef:
     return ref("domain_event/{0}", domain_event_id)
+
+
+def create_domain_event_from_description(description: str) -> DomainEvent:
+    return DomainEvent(
+        id=domain_event_ref(str(uuid.uuid4())),
+        description=description,
+    )
+
+
+def find_all_domain_events() -> Callable[[Graph], List[DomainEvent]]:
+    def query(graph: Graph) -> List[DomainEvent]:
+        results = graph.query(
+            f"""
+            SELECT ?event ?description WHERE {{
+                ?event a <{ref("types/domain_event")}> .
+                ?event <{ref("types/domain_event/description")}> ?description .
+            }}
+            """
+        )
+
+        domain_events = []
+        for result in results:
+            domain_events.append(
+                DomainEvent(id=result.event, description=str(result.description))
+            )
+
+        return domain_events
+
+    return query
+
+
+def add_domain_event_command(domain_event: DomainEvent) -> Callable[[Graph], None]:
+    def command(g: Graph):
+        g.add((domain_event.id, RDF.type, ref("types/domain_event")))
+        g.add(
+            (
+                domain_event.id,
+                ref("types/domain_event/description"),
+                Literal(domain_event.description),
+            )
+        )
+
+    return command
+
+
+def remove_domain_event_command(domain_event_id: URIRef) -> Callable[[Graph], None]:
+    def command(g: Graph):
+        g.remove((domain_event_id, None, None))
+
+    return command
--- test/domain/test_event.py
+++ test/domain/test_event.py
@@ -1,6 +1,14 @@
 from rdflib import URIRef
 
-from project_reasoner.domain.event import DomainEvent, domain_event_ref
+from project_reasoner.domain.event import (
+    DomainEvent,
+    domain_event_ref,
+    find_all_domain_events,
+    create_domain_event_from_description,
+    add_domain_event_command,
+    remove_domain_event_command,
+)
+from project_reasoner.knowledge_graph import KnowledgeGraph
 
 
 def test_a_domain_event_has_an_id():
@@ -13,3 +21,48 @@ def test_a_domain_event_has_a_description():
     event_id = domain_event_ref("123")
     event = DomainEvent(id=event_id, description="Domain event description")
     assert event.description == "Domain event description"
+
+
+def test_we_start_with_no_domain_events():
+    knowledge = KnowledgeGraph()
+    goals = knowledge.query(find_all_domain_events())
+    assert len(goals) == 0
+
+
+def test_we_can_add_and_retrieve_one_domain_event():
+    knowledge = KnowledgeGraph()
+    knowledge.update(
+        add_domain_event_command(
+            create_domain_event_from_description("Money was spent on a bank account")
+        )
+    )
+    goals = knowledge.query(find_all_domain_events())
+    assert len(goals) == 1
+    assert goals[0].description == "Money was spent on a bank account"
+
+
+def test_we_can_add_and_retrieve_multiple_domain_events():
+    knowledge = KnowledgeGraph()
+    knowledge.update(
+        add_domain_event_command(
+            create_domain_event_from_description("Money was spent on a bank account")
+        )
+    )
+    knowledge.update(
+        add_domain_event_command(
+            create_domain_event_from_description(
+                "Money was earned and received on a bank account"
+            )
+        )
+    )
+    goals = knowledge.query(find_all_domain_events())
+    assert len(goals) == 2
+
+
+def test_we_can_remove_a_domain_event():
+    domain = create_domain_event_from_description("Money was spent on a bank account")
+    knowledge = KnowledgeGraph()
+    knowledge.update(add_domain_event_command(domain))
+    knowledge.update(remove_domain_event_command(domain.id))
+    goals = knowledge.query(find_all_domain_events())
+    assert len(goals) == 0
